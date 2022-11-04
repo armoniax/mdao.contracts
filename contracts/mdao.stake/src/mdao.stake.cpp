e910492c91366c5f22750607f5da38420fed8f70
rsxlj <113492841+rsxlj@users.noreply.github.com>
2022-11-04 16:11:57 +0800

    fix stake contract

--- contracts/mdao.stake/src/mdao.stake.cpp
+++ contracts/mdao.stake/src/mdao.stake.cpp
@@ -68,7 +68,7 @@ void mdaostake::staketoken(const name& from, const name& to, const asset& quanti
     _db.set(dao_stake, get_self());
 }
 
-ACTION mdaostake::unlocktoken(const uint64_t &id, const vector<extended_asset> &tokens)
+ACTION mdaostake::unstaketoken(const uint64_t &id, const vector<extended_asset> &tokens)
 {
     CHECKC(_gstate.initialized, stake_err::UNINITIALIZED, "contract uninitialized");
     user_stake_t user_stake(id);
@@ -87,7 +87,7 @@ ACTION mdaostake::unlocktoken(const uint64_t &id, const vector<extended_asset> &
         extended_asset token = *out_iter;
         extended_symbol sym = token.get_extended_symbol();
         CHECKC(token.quantity.amount > 0 && token.quantity.is_valid(), stake_err::INVALID_PARAMS, "invalid amount");
-        CHECKC(token.quantity.amount <= user_stake.tokens_stake[sym], stake_err::unstake_OVERFLOW, "stake amount not enough");
+        CHECKC(token.quantity.amount <= user_stake.tokens_stake[sym], stake_err::UNLOCK_OVERFLOW, "stake amount not enough");
         user_stake.tokens_stake[sym] =
             (safe<int64_t>(dao_stake.tokens_stake[sym]) - safe<int64_t>(token.quantity.amount)).value;
         dao_stake.tokens_stake[sym] = 
